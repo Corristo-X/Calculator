29625a1d5440835a13c003f8ded63e61f47862e0
Daniel <heinkokowoko@gmail.com>
2022-07-06 18:38:02 +0200

    [OWL-1] Change Color

--- calculator/App.js
+++ calculator/App.js
@@ -102,7 +102,7 @@ const styles = StyleSheet.create({
     
     
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: 'orange',
     alignItems: 'center',
     justifyContent:'flex-end',
   },
